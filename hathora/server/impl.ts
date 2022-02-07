0264fa3294e0ce2e82aa90e185a14ee22a9f9b44
Harsh Pandey <hpandey793@gmail.com>
2022-02-07 14:55:52 -0500

    hathora backend

--- hathora/server/impl.ts
+++ hathora/server/impl.ts
@@ -1,20 +1,52 @@
 import { Methods, Context } from "./.hathora/methods";
 import { Response } from "../api/base";
-import { PlayerState, UserId, ISetDirectionRequest } from "../api/types";
+import { PlayerState, UserId, ISetDirectionRequest, Point, Direction } from "../api/types";
 
-type InternalState = {};
+type Player = {
+  id: UserId;
+  x: number;
+  y: number;
+  direction: Direction;
+};
+type InternalState = {
+  players: Player[];
+};
+
+const SPEED = 200;
 
 export class Impl implements Methods<InternalState> {
   initialize(userId: UserId, ctx: Context): InternalState {
-    return {};
-  }
-  setDirection(state: InternalState, userId: UserId, ctx: Context, request: ISetDirectionRequest): Response {
-    return Response.error("Not implemented");
-  }
-  getUserState(state: InternalState, userId: UserId): PlayerState {
     return {
       players: [],
     };
   }
-  onTick(state: InternalState, ctx: Context, timeDelta: number): void {}
+  setDirection(state: InternalState, userId: UserId, ctx: Context, request: ISetDirectionRequest): Response {
+    const player = state.players.find((p) => p.id === userId);
+    if (player === undefined) {
+      const x = ctx.chance.natural({ max: 1280 - 1 });
+      const y = ctx.chance.natural({ max: 720 - 1 });
+      state.players.push({ id: userId, x, y, direction: request.direction });
+    } else {
+      player.direction = request.direction;
+    }
+    return Response.ok();
+  }
+  getUserState(state: InternalState, userId: UserId): PlayerState {
+    return {
+      players: state.players.map((player) => ({ x: player.x, y: player.y })),
+    };
+  }
+  onTick(state: InternalState, ctx: Context, timeDelta: number): void {
+    state.players.forEach((player) => {
+      if (player.direction === Direction.UP) {
+        player.y -= SPEED * timeDelta;
+      } else if (player.direction === Direction.DOWN) {
+        player.y += SPEED * timeDelta;
+      } else if (player.direction === Direction.LEFT) {
+        player.x -= SPEED * timeDelta;
+      } else if (player.direction === Direction.RIGHT) {
+        player.x += SPEED * timeDelta;
+      }
+    });
+  }
 }
